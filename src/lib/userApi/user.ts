afe0b0f0e5bd71c215b09401ed9b49cc300cda56
tecnology-expart <mdhakimshorkar123@gmail.com>
2025-10-06 09:37:30 +0600

    es-lint-problem-solve

--- src/lib/userApi/user.ts
+++ src/lib/userApi/user.ts
@@ -1,23 +1,9 @@
-// import api from "../api";
-
-
-// export const getAllUsers = async () => {
-//   try {
-//     const res = await api.get(`/auth/${query}`);
-//     return res.data; 
-//   } catch (error: any) {
-//     console.error("Get All Users Error:", error.response?.data || error.message);
-//     throw error;
-//   }
-// };
-
-
 
 import api from "../api";
 
 export const getAllUsers = async (query: string) => {
   try {
-    const res = await api.get(`/auth${query}`); // ✅ query ঠিকভাবে লাগানো হলো
+    const res = await api.get(`/auth${query}`);
     return res.data;
   } catch (error: any) {
     console.error("Get All Users Error:", error.response?.data || error.message);
