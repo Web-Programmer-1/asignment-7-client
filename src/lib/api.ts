cfccdb14db422fc3e478908648c4c5425d8cb132
tecnology-expart <mdhakimshorkar123@gmail.com>
2025-10-06 09:45:00 +0600

    added-base-url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,11 +3,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://a-7-portfolio-backend.vercel.app/api/v1",
   withCredentials: true, 
 });
 
 export default api;
+
 
 
 
