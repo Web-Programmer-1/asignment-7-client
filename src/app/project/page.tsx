707993645becefa1205d71def5071d3f7635f311
tecnology-expart <mdhakimshorkar123@gmail.com>
2025-10-07 08:56:16 +0600

    added-utl

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -228,7 +228,7 @@ export default function ProjectList() {
                           className="mt-3 inline-flex items-center justify-center gap-2 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:from-blue-600 hover:to-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md px-4 py-2 text-md transition-all duration-300 shadow-md hover:shadow-lg"
                         >
                           <VscLiveShare className="text-lg" />
-                          Live
+                          Live.
                         </Link>
 
                         {/* views More BTn */}
